93e833f66ab56a2ceeadd5cc085ba07bcfeb734a
Natacha <natachaliao32@gmail.com>
2021-06-06 13:40:48 +0200

    hash router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Switch , Route } from 'react-router-dom';
+import { HashRouter as Router, Switch , Route } from 'react-router-dom';
 import { Homepage } from './homepage/Homepage';
 import Header from './all/Header';
 import { Contact } from './all/Contact';
