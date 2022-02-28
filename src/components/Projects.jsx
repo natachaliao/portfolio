7b34b07da6a1af6e7e5f4e844dfa075c9511001c
Natacha <natachaliao32@gmail.com>
2022-02-28 20:39:43 +0100

    correct placement project hover

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -30,34 +30,6 @@ const Project = (props) => {
     });
   }
 
-  const addPattern = (svg, idName, patternToAdd) => {
-
-    let id = `pattern-${idName}`;
-    var svgNS = svg.namespaceURI;
-
-    var pattern = document.createElementNS(svgNS, 'pattern');
-
-    pattern.setAttributeNS(svgNS, 'patternUnits', 'userSpaceOnUse');
-    pattern.id = id;
-    pattern.style.width = '1200';
-    pattern.style.height = '600';
-
-    // var image = document.createElementNS(svgNS, 'image');
-    // image.style.x = 0;
-    // image.style.y = 0;
-    // image.style.width = '1200';
-    // image.style.height = '600';
-
-    pattern.appendChild(patternToAdd);
-
-    var defs = svg.querySelector('defs') ||
-        svg.insertBefore(document.createElementNS(svgNS, 'defs'), svg.firstChild);
-
-    svg.querySelector('path').style.fill = 'url(#' + id + ')';
-
-    return defs.appendChild(pattern);
-  }
-
   const changePattern = async (svg, idName, path) => {
     let id = `pattern-${idName}`;
     let pattern = svg.querySelector('pattern');
@@ -73,23 +45,10 @@ const Project = (props) => {
     framedPicture.setAttribute('fill', 'url(#' + id + ')');
   }
 
-  const createInfoOnHover = () => {
-    (
-      <div className="projprev-hover">
-        <h3 className="projprev-title">{name}</h3>
-        <div className="projprev-categories">
-          {categories.map((category, index) => <span key={`projprev-category-${index}`}>{category}</span>)}
-        </div>
-      </div>
-    )
-  }
-
   useEffect(() => {
     animateProject();
     const svg = document.querySelector(`#frame-${idName}`)
     changePattern(svg, idName, path);
-    
-    // addPattern(svg, `info-${idName}`, createInfoOnHover());
   })
 
   return (
@@ -133,9 +92,6 @@ const ProjectHover = (props) => {
 
   useEffect(() => {
     placeProjectHover(`projprev-${idName}`);
-  }, [])
-
-  useEffect(() => {
     window.addEventListener('resize', () => placeProjectHover(`projprev-${idName}`))
   })
 
